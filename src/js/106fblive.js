11b15a2989bd53b08be87982dd33500b6e6e71f5
AsoTiesto <cute17852337569@gmail.com>
2018-03-05 17:02:11 +0800

    follow #3407 修復及更新活動頁

--- src/js/106fblive.js
+++ src/js/106fblive.js
@@ -80,84 +80,125 @@ $(function () {
       }
     })
   }
-  normal()
-  leftSideImgTarget.css('opacity', '0.4')
 
-  $('.teacher-condition img').on('click', event => {
-    let thisBtn = $(event.currentTarget)
-    let thisImgTitle = thisBtn.attr('title')
-    let otherImgTitle = thisBtn.siblings('[data-clicked=true]').attr('title')
-    let chA, chB, chC, chD,
-      enA, enB, enC, enD,
-      maA, maB, maC, maD,
-      pcA, pcB, pcC, pcD,
-      soA, soB, soC, soD
+  let clickTeacherImg = () => {
+    $('.teacher-condition img').on('click', event => {
+      let thisBtn = $(event.currentTarget)
+      let thisImgTitle = thisBtn.attr('title')
+      let otherImgTitle = thisBtn.siblings('[data-clicked=true]').attr('title')
+      let chA, chB, chC, chD,
+        enA, enB, enC, enD,
+        maA, maB, maC, maD,
+        pcA, pcB, pcC, pcD,
+        soA, soB, soC, soD
 
-    leftSideImgTarget.css('opacity', '1')
-    $('.left-side .left1').css('opacity', '0.4')
-    $('.youtube-video iframe').remove()
+      leftSideImgTarget.css('opacity', '1')
+      $('.left-side .left1').css('opacity', '0.4')
+      $('.youtube-video iframe').remove()
 
-    if (thisImgTitle === '敬請期待') {
-      leftSideImgTarget.css('opacity', '0.4')
+      if (thisImgTitle === '敬請期待') {
+        leftSideImgTarget.css('opacity', '0.4')
+        thisBtn.siblings('[data-clicked=true]').attr('src', `${rootPath}/img/${otherImgTitle}正常.png`)
+        return false
+      } else if (thisImgTitle === '柳吟國文') {
+        chA = 'https://www.youtube.com/embed/cFbgZS33w_A'
+        chB = 'https://www.youtube.com/embed/7seqN-mlXrM'
+        chC = 'https://www.youtube.com/embed/j5SaFJwGAGE'
+        chD = 'https://www.youtube.com/embed/OL7t4_ZFLHU'
+
+        videoTarget.append(
+          `<iframe class="ch" width=${widthStyle} height=${heightStyle} src=${chA}></iframe>`
+        )
+        changeVideo(chA, chB, chC, chD)
+      } else if (thisImgTitle === '東方智英文') {
+        enA = 'https://www.youtube.com/embed/_kqZYBGiOc4'
+        enB = 'https://www.youtube.com/embed/r0haWB1Uezk'
+        enC = 'https://www.youtube.com/embed/rbhm6UFuGIM'
+        enD = 'https://www.youtube.com/embed/3hzX-BWfark'
+
+        videoTarget.append(
+          `<iframe class="en" width=${widthStyle} height=${heightStyle} src=${enA}></iframe>`
+        )
+        changeVideo(enA, enB, enC, enD)
+      } else if (thisImgTitle === '楊明山數學') {
+        maA = 'https://www.youtube.com/embed/9m02FHN8c-k'
+        maB = 'https://www.youtube.com/embed/SKoJRc_NlUw'
+        maC = 'https://www.youtube.com/embed/8v_yDZ7E_PA'
+        maD = 'https://www.youtube.com/embed/4KUkaMeg_aI'
+
+        videoTarget.append(
+          `<iframe class="ma" width=${widthStyle} height=${heightStyle} src=${maA}></iframe>`
+        )
+        changeVideo(maA, maB, maC, maD)
+      } else if (thisImgTitle === '紀揚理化') {
+        pcA = 'https://www.youtube.com/embed/ig8KSgTqnb8'
+        pcB = 'https://www.youtube.com/embed/c_J08o4hJ88'
+        pcC = 'https://www.youtube.com/embed/bEceCiqRJOk'
+        pcD = 'https://www.youtube.com/embed/3FiH9K9F2U0'
+
+        videoTarget.append(
+          `<iframe class="pc" width=${widthStyle} height=${heightStyle} src=${pcA}></iframe>`
+        )
+        changeVideo(pcA, pcB, pcC, pcD)
+      } else if (thisImgTitle === '李天豪社會') {
+        soA = 'https://www.youtube.com/embed/VMujFZ2kuQk'
+        soB = 'https://www.youtube.com/embed/wjsk1wHM1vg'
+        soC = 'https://www.youtube.com/embed/aaCgzRFno1U'
+        soD = 'https://www.youtube.com/embed/IPkwT6GADWQ'
+
+        videoTarget.append(
+          `<iframe class="so" width=${widthStyle} height=${heightStyle} src=${soA}></iframe>`
+        )
+        changeVideo(soA, soB, soC, soD)
+      }
+
+      thisBtn.attr('data-clicked', true)
+      thisBtn.attr('src', `${rootPath}/img/${thisImgTitle}點選.png`)
       thisBtn.siblings('[data-clicked=true]').attr('src', `${rootPath}/img/${otherImgTitle}正常.png`)
+      thisBtn.siblings('[data-clicked=true]').removeAttr('data-clicked')
+
       return false
-    } else if (thisImgTitle === '柳吟國文') {
-      chA = 'https://www.youtube.com/embed/cFbgZS33w_A'
-      chB = 'https://www.youtube.com/embed/7seqN-mlXrM'
-      chC = 'https://www.youtube.com/embed/j5SaFJwGAGE'
-      chD = 'https://www.youtube.com/embed/OL7t4_ZFLHU'
+    })
+  }
 
-      videoTarget.append(
-        `<iframe class="ch" width=${widthStyle} height=${heightStyle} src=${chA}></iframe>`
-      )
-      changeVideo(chA, chB, chC, chD)
-    } else if (thisImgTitle === '東方智英文') {
-      enA = 'https://www.youtube.com/embed/_kqZYBGiOc4'
-      enB = 'https://www.youtube.com/embed/r0haWB1Uezk'
-      enC = 'https://www.youtube.com/embed/rbhm6UFuGIM'
-      enD = 'https://www.youtube.com/embed/3hzX-BWfark'
+  let blockFunc = info => {
+    $.blockUI({
+      message: `<img src='${rootPath}/img/開通步驟.png'>`,
+      css: {
+        top: ($(window).height() - 600) / 2 + 'px',
+        left: ($(window).width() - 500) / 2 + 'px',
+        padding: '0px',
+        width: '600',
+        height: '600',
+        cursor: 'default'
+      },
+      overlayCSS: {
+        backgroundColor: '#000',
+        opacity: '0.6'
+      },
+      onOverlayClick: $.unblockUI
+    })
+  }
 
-      videoTarget.append(
-        `<iframe class="en" width=${widthStyle} height=${heightStyle} src=${enA}></iframe>`
-      )
-      changeVideo(enA, enB, enC, enD)
-    } else if (thisImgTitle === '楊明山數學') {
-      maA = 'https://www.youtube.com/embed/9m02FHN8c-k'
-      maB = 'https://www.youtube.com/embed/SKoJRc_NlUw'
-      maC = 'https://www.youtube.com/embed/8v_yDZ7E_PA'
-      maD = 'https://www.youtube.com/embed/4KUkaMeg_aI'
+  $(document).ready(() => {
+    $('.teacher-condition .en').trigger('click')
+  })
 
-      videoTarget.append(
-        `<iframe class="ma" width=${widthStyle} height=${heightStyle} src=${maA}></iframe>`
-      )
-      changeVideo(maA, maB, maC, maD)
-    } else if (thisImgTitle === '紀揚理化') {
-      pcA = 'https://www.youtube.com/embed/ig8KSgTqnb8'
-      pcB = 'https://www.youtube.com/embed/c_J08o4hJ88'
-      pcC = 'https://www.youtube.com/embed/bEceCiqRJOk'
-      pcD = 'https://www.youtube.com/embed/3FiH9K9F2U0'
-
-      videoTarget.append(
-        `<iframe class="pc" width=${widthStyle} height=${heightStyle} src=${pcA}></iframe>`
-      )
-      changeVideo(pcA, pcB, pcC, pcD)
-    } else if (thisImgTitle === '李天豪社會') {
-      soA = 'https://www.youtube.com/embed/VMujFZ2kuQk'
-      soB = 'https://www.youtube.com/embed/wjsk1wHM1vg'
-      soC = 'https://www.youtube.com/embed/aaCgzRFno1U'
-      soD = 'https://www.youtube.com/embed/IPkwT6GADWQ'
-
-      videoTarget.append(
-        `<iframe class="so" width=${widthStyle} height=${heightStyle} src=${soA}></iframe>`
-      )
-      changeVideo(soA, soB, soC, soD)
-    }
-
-    thisBtn.attr('data-clicked', true)
-    thisBtn.attr('src', `${rootPath}/img/${thisImgTitle}點選.png`)
-    thisBtn.siblings('[data-clicked=true]').attr('src', `${rootPath}/img/${otherImgTitle}正常.png`)
-    thisBtn.siblings('[data-clicked=true]').removeAttr('data-clicked')
-
+  $('.share .fb-share').on('click', event => {
+    window.open('https://www.facebook.com/ehanlin.com.tw/posts/1632313766827783', 'fb分享')
     return false
   })
+
+  $('.fb .fb-search').on('click', event => {
+    window.open('https://www.facebook.com/ehanlin.com.tw/', 'ehanlinFb')
+    return false
+  })
+
+  $('.open .how-open').on('click', event => {
+    blockFunc()
+  })
+
+  normal()
+  leftSideImgTarget.css('opacity', '0.4')
+  clickTeacherImg()
 })
