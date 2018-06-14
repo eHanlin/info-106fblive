d6f1bb73b0141dcc2f3f0204a6489be38fdc3887
AsoTiesto <cute17852337569@gmail.com>
2018-06-14 19:12:23 +0800

    新增功能：完成使用 gcs.sh 來 upload gcp 功能（測試機）

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,29 @@
+const Q = require('q')
+const del = require('del')
 const gulp = require('gulp')
-const templateUtil = require('gulp-template-util')
 const babel = require('gulp-babel')
 const uglify = require('gulp-uglify-es').default
-const del = require('del')
-const Q = require('q')
+const gcPub = require('gulp-gcloud-publish')
+const Storage = require('@google-cloud/storage')
+const templateUtil = require('gulp-template-util')
+
+let bucketName = 'tutor-events'
+let projectId = 'tutor-204108'
+let keyFilename = './tutor.json'
+let projectName = '106fblive'
+
+const storage = new Storage({
+  projectId: projectId,
+  keyFilename: keyFilename
+})
 
 const basePath = {
   base: 'src'
 }
 const dist = 'dist'
 
-function copyStaticTask (destination) {
-  return function () {
+let copyStaticTask = destination => {
+  return () => {
     return gulp
       .src(
         ['src/**/*.html', 'src/img/**/*', 'src/lib/**/*', 'src/js/**/*.js', 'src/css/**/*.css'], {
@@ -22,14 +34,14 @@ function copyStaticTask (destination) {
   }
 }
 
-function clean (source) {
-  return function () {
+let clean = source => {
+  return () => {
     return del([source])
   }
 }
 
-function minifyJs (sourceJS) {
-  return function () {
+let minifyJs = sourceJS => {
+  return () => {
     return gulp
       .src(sourceJS, {
         base: 'babel-temp'
@@ -45,8 +57,8 @@ function minifyJs (sourceJS) {
   }
 }
 
-function babelJS (sourceJS) {
-  return function () {
+let babelJS = sourceJS => {
+  return () => {
     return gulp
       .src(sourceJS, basePath)
       .pipe(babel())
@@ -54,7 +66,7 @@ function babelJS (sourceJS) {
   }
 }
 
-function buildJS () {
+let buildJS = () => {
   let deferred = Q.defer()
 
   Q.fcall(function () {
@@ -66,22 +78,57 @@ function buildJS () {
     .then(function () {
       return templateUtil.logPromise(clean('babel-temp'))
     })
-
   return deferred.promise
 }
 
+let removeEmptyFiles = () => {
+  let array = ['img', 'css', 'js', 'lib']
+  array.forEach(emptyFiles => {
+    storage
+      .bucket(bucketName)
+      .file(`/event/${projectName}/${emptyFiles}`)
+      .delete()
+      .then(() => {
+        console.log(`gs://${bucketName}/${emptyFiles} deleted.`)
+      })
+      .catch(err => {
+        console.error('ERROR:', err)
+      })
+  })
+}
+
+gulp.task('uploadGcp', () => {
+  return gulp.src(['dist/**/*'])
+    .pipe(gcPub({
+      bucket: bucketName,
+      keyFilename: keyFilename,
+      projectId: projectId,
+      base: `/event/${projectName}`,
+      public: true,
+      transformDestination: path => {
+        return path
+      },
+      metadata: {
+        cacheControl: 'max-age=315360000, no-transform, public'
+      }
+    }))
+})
+
+gulp.task('removeEmptyFiles', () => {
+  removeEmptyFiles()
+})
+
 gulp.task('minifyJs', minifyJs('src/js/**/*.js'))
-gulp.task('package', function () {
-  var deferred = Q.defer()
-  Q.fcall(function () {
+gulp.task('package', () => {
+  let deferred = Q.defer()
+  Q.fcall(() => {
     return templateUtil.logPromise(clean(dist))
   })
-    .then(function () {
+    .then(() => {
       return templateUtil.logStream(copyStaticTask('dist'))
     })
-    .then(function () {
+    .then(() => {
       return Q.all([templateUtil.logStream(buildJS)])
     })
-
   return deferred.promise
 })
