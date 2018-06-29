979239ee317e884e48f014ffecfdf4f98e7e1f98
AsoTiesto <cute17852337569@gmail.com>
2018-06-29 15:19:38 +0800

    調整功能：整理 code

--- src/js/106fblive.js
+++ src/js/106fblive.js
@@ -1,90 +1,59 @@
 define(['jquery', 'jqueryBlockUI'], ($, jqueryBlockUI) => {
-  let rootPath = `https://s3-ap-northeast-1.amazonaws.com/ehanlin-web-resource/info-106fblive`
+  let s3Path = `https://s3-ap-northeast-1.amazonaws.com/ehanlin-web-resource/info-106fblive/img/`
   let youtubeUrl = `https://www.youtube.com/embed/`
-  // let rootPath = '.'
-  // let nowSeconds = new Date().getTime() / 1000
-  // let date0309 = 1520568000
-  // let date0316 = 1521172800
-  // let date0323 = 1521777600
   let subjectTarget = $('.container .teacher-condition')
   let leftSideImgTarget = $('.left-side img')
   let videoTarget = $('.youtube-video')
-  let widthStyle = '560'
-  let heightStyle = '345'
+  let videoWid = '560'
+  let videoHei = '345'
+  let titleArray = ['柳吟國文', '東方智英文', '楊明山數學', '紀揚理化', '李天豪社會']
+  let contentArray = ['爆中考點', '進階B', '躍升A', '衝刺A']
+  let subArray = ['ch', 'en', 'ma', 'pc', 'so']
   let normal = () => {
-    subjectTarget.append(
-      `<img class="ch" title="敬請期待" style="opacity:0.6;" src="${rootPath}/img/柳吟國文敬請期待.png">
-          <img class="en" title="敬請期待" style="opacity:0.6;" src="${rootPath}/img/東方智英文敬請期待.png">
-          <img class="ma" title="敬請期待" style="opacity:0.6;" src="${rootPath}/img/楊明山數學敬請期待.png">
-          <img class="pc" title="敬請期待" style="opacity:0.6;" src="${rootPath}/img/紀揚理化敬請期待.png">
-          <img class="so" title="敬請期待" style="opacity:0.6;" src="${rootPath}/img/李天豪社會敬請期待.png">`
-    )
-
     /* 判斷當前協議是http還是https */
-    if (window.location.protocol != 'https:') {
+    if (window.location.protocol !== 'https:') {
       window.location.href = 'https:' + window.location.href.substring(window.location.protocol.length)
     }
 
-    /* nowSeconds >= date0309 */
-    // if (true) {
-    $('.container .teacher-condition .ch').remove()
-    subjectTarget.append(
-      `<img class="ch" title="柳吟國文" src="${rootPath}/img/柳吟國文正常.png">`
-    )
-    // }
-    /* nowSeconds >= date0316 */
-    // if (true) {
-    $('.container .teacher-condition .en').remove()
-    $('.container .teacher-condition .ma').remove()
-    subjectTarget.append(
-      `<img class="en" title="東方智英文" src="${rootPath}/img/東方智英文正常.png">`
-    )
-    subjectTarget.append(
-      `<img class="ma" title="楊明山數學" src="${rootPath}/img/楊明山數學正常.png">`
-    )
-    // }
-    /* nowSeconds >= date0323 */
-    // if (true) {
-    $('.container .teacher-condition .pc').remove()
-    $('.container .teacher-condition .so').remove()
-    subjectTarget.append(
-      `<img class="pc" title="紀揚理化" src="${rootPath}/img/紀揚理化正常.png">`
-    )
-    subjectTarget.append(
-      `<img class="so" title="李天豪社會" src="${rootPath}/img/李天豪社會正常.png">`
-    )
-    // }
+    $('.container .teacher-condition img').remove()
+    for (let index = 0; index < titleArray.length; index++) {
+      subjectTarget.append(
+        `<img class=${subArray[index]} title=${titleArray[index]} src="${s3Path}${titleArray[index]}正常.png">`
+      )
+    }
   }
 
   let changeVideo = (youtubeUrlA, youtubeUrlB, youtubeUrlC, youtubeUrlD) => {
     leftSideImgTarget.on('click', event => {
       let thisBtn = $(event.currentTarget)
       let thisImgTitle = thisBtn.attr('title')
+      let youtubeUri, number
 
       leftSideImgTarget.css('opacity', '1')
       $('.youtube-video iframe').remove()
 
-      if (thisImgTitle === '爆中考點') {
-        $('.left-side .left1').css('opacity', '0.4')
-        videoTarget.append(
-          `<iframe width=${widthStyle} height=${heightStyle} src=${youtubeUrlA}></iframe>`
-        )
-      } else if (thisImgTitle === '進階B') {
-        $('.left-side .left2').css('opacity', '0.4')
-        videoTarget.append(
-          `<iframe width=${widthStyle} height=${heightStyle} src=${youtubeUrlB}></iframe>`
-        )
-      } else if (thisImgTitle === '躍升A') {
-        $('.left-side .left3').css('opacity', '0.4')
-        videoTarget.append(
-          `<iframe width=${widthStyle} height=${heightStyle} src=${youtubeUrlC}></iframe>`
-        )
-      } else if (thisImgTitle === '衝刺A') {
-        $('.left-side .left4').css('opacity', '0.4')
-        videoTarget.append(
-          `<iframe width=${widthStyle} height=${heightStyle} src=${youtubeUrlD}></iframe>`
-        )
+      switch (thisImgTitle) {
+        case contentArray[0]:
+          number = 1
+          youtubeUri = youtubeUrlA
+          break
+        case contentArray[1]:
+          number = 2
+          youtubeUri = youtubeUrlB
+          break
+        case contentArray[2]:
+          number = 3
+          youtubeUri = youtubeUrlC
+          break
+        case contentArray[3]:
+          number = 4
+          youtubeUri = youtubeUrlD
+          break
+        default:
+          break
       }
+      $(`.left-side .left${number}`).css('opacity', '0.4')
+      videoTarget.append(`<iframe width=${videoWid} height=${videoHei} src=${youtubeUri}></iframe>`)
     })
   }
 
@@ -93,78 +62,67 @@ define(['jquery', 'jqueryBlockUI'], ($, jqueryBlockUI) => {
       let thisBtn = $(event.currentTarget)
       let teacherImgTitle = thisBtn.attr('title')
       let otherImgTitle = thisBtn.siblings('[data-clicked=true]').attr('title')
-      let chA, chB, chC, chD,
-        enA, enB, enC, enD,
-        maA, maB, maC, maD,
-        pcA, pcB, pcC, pcD,
-        soA, soB, soC, soD
+      let chA, chB, chC, chD, enA, enB, enC, enD, maA, maB, maC, maD,
+        pcA, pcB, pcC, pcD, soA, soB, soC, soD, subNum, sub
 
       leftSideImgTarget.css('opacity', '1')
       $('.left-side .left1').css('opacity', '0.4')
       $('.youtube-video iframe').remove()
 
-      if (teacherImgTitle === '敬請期待') {
-        leftSideImgTarget.css('opacity', '0.4')
-        thisBtn.siblings('[data-clicked=true]').attr('src', `${rootPath}/img/${otherImgTitle}正常.png`)
-        $('.left-side img').hide()
-        return false
-      } else if (teacherImgTitle === '柳吟國文') {
-        chA = `${youtubeUrl}-DRq12BK2eM`
-        chB = `${youtubeUrl}sa5gf7fzNHg`
-        chC = `${youtubeUrl}L1PT97XoeOg`
-        chD = `${youtubeUrl}XDh-5Z-zVBY`
-
-        videoTarget.append(
-          `<iframe class="ch" width=${widthStyle} height=${heightStyle} src=${chA}></iframe>`
-        )
-        changeVideo(chA, chB, chC, chD)
-      } else if (teacherImgTitle === '東方智英文') {
-        enA = `${youtubeUrl}SqkoYOueFsU`
-        enB = `${youtubeUrl}7RtCbGPrGSA`
-        enC = `${youtubeUrl}2BTlYPBZ8M8`
-        enD = `${youtubeUrl}X5k5tkBwgCE`
-
-        $('.left-side img').show()
-        videoTarget.append(
-          `<iframe class="en" width=${widthStyle} height=${heightStyle} src=${enA}></iframe>`
-        )
-        changeVideo(enA, enB, enC, enD)
-      } else if (teacherImgTitle === '楊明山數學') {
-        maA = `${youtubeUrl}k7tf2CtMuc8`
-        maB = `${youtubeUrl}qLjNp1AqTLU`
-        maC = `${youtubeUrl}cPlqMux310k`
-        maD = `${youtubeUrl}6X4zOFBLk-s`
-
-        videoTarget.append(
-          `<iframe class="ma" width=${widthStyle} height=${heightStyle} src=${maA}></iframe>`
-        )
-        changeVideo(maA, maB, maC, maD)
-      } else if (teacherImgTitle === '紀揚理化') {
-        pcA = `${youtubeUrl}r6IWQt_Nzm4`
-        pcB = `${youtubeUrl}6NUJjWIFTFc`
-        pcC = `${youtubeUrl}l9Y5YfK89x0`
-        pcD = `${youtubeUrl}2rwrdu1e5vQ`
-
-        videoTarget.append(
-          `<iframe class="pc" width=${widthStyle} height=${heightStyle} src=${pcA}></iframe>`
-        )
-        changeVideo(pcA, pcB, pcC, pcD)
-      } else if (teacherImgTitle === '李天豪社會') {
-        soA = `${youtubeUrl}fWWITYky4j0`
-        soB = `${youtubeUrl}BpEUEMq8IHc`
-        soC = `${youtubeUrl}z7Ce90S8eyQ`
-        soD = `${youtubeUrl}Go55OciG1Qc`
-
-        $('.left-side img').show()
-        videoTarget.append(
-          `<iframe class="so" width=${widthStyle} height=${heightStyle} src=${soA}></iframe>`
-        )
-        changeVideo(soA, soB, soC, soD)
+      switch (teacherImgTitle) {
+        case titleArray[0]:
+          chA = `${youtubeUrl}-DRq12BK2eM`
+          chB = `${youtubeUrl}sa5gf7fzNHg`
+          chC = `${youtubeUrl}L1PT97XoeOg`
+          chD = `${youtubeUrl}XDh-5Z-zVBY`
+          subNum = chA
+          sub = subArray[0]
+          changeVideo(chA, chB, chC, chD)
+          break
+        case titleArray[1]:
+          enA = `${youtubeUrl}SqkoYOueFsU`
+          enB = `${youtubeUrl}7RtCbGPrGSA`
+          enC = `${youtubeUrl}2BTlYPBZ8M8`
+          enD = `${youtubeUrl}X5k5tkBwgCE`
+          subNum = enA
+          sub = subArray[1]
+          changeVideo(enA, enB, enC, enD)
+          break
+        case titleArray[2]:
+          maA = `${youtubeUrl}k7tf2CtMuc8`
+          maB = `${youtubeUrl}qLjNp1AqTLU`
+          maC = `${youtubeUrl}cPlqMux310k`
+          maD = `${youtubeUrl}6X4zOFBLk-s`
+          subNum = maA
+          sub = subArray[2]
+          changeVideo(maA, maB, maC, maD)
+          break
+        case titleArray[3]:
+          pcA = `${youtubeUrl}r6IWQt_Nzm4`
+          pcB = `${youtubeUrl}6NUJjWIFTFc`
+          pcC = `${youtubeUrl}l9Y5YfK89x0`
+          pcD = `${youtubeUrl}2rwrdu1e5vQ`
+          subNum = pcA
+          sub = subArray[3]
+          changeVideo(pcA, pcB, pcC, pcD)
+          break
+        case titleArray[4]:
+          soA = `${youtubeUrl}fWWITYky4j0`
+          soB = `${youtubeUrl}BpEUEMq8IHc`
+          soC = `${youtubeUrl}z7Ce90S8eyQ`
+          soD = `${youtubeUrl}Go55OciG1Qc`
+          subNum = soA
+          sub = subArray[4]
+          changeVideo(soA, soB, soC, soD)
+          break
+        default:
+          break
       }
+      videoTarget.append(`<iframe class=${sub} width=${videoWid} height=${videoHei} src=${subNum}></iframe>`)
 
       thisBtn.attr('data-clicked', true)
-      thisBtn.attr('src', `${rootPath}/img/${teacherImgTitle}點選.png`)
-      thisBtn.siblings('[data-clicked=true]').attr('src', `${rootPath}/img/${otherImgTitle}正常.png`)
+      thisBtn.attr('src', `${s3Path}${teacherImgTitle}點選.png`)
+      thisBtn.siblings('[data-clicked=true]').attr('src', `${s3Path}${otherImgTitle}正常.png`)
       thisBtn.siblings('[data-clicked=true]').removeAttr('data-clicked')
 
       return false
@@ -173,7 +131,7 @@ define(['jquery', 'jqueryBlockUI'], ($, jqueryBlockUI) => {
 
   let blockFunc = info => {
     $.blockUI({
-      message: `<img src='${rootPath}/img/開通步驟編修檔.png'>`,
+      message: `<img src='${s3Path}開通步驟編修檔.png'>`,
       css: {
         top: ($(window).height() - 600) / 2 + 'px',
         left: ($(window).width() - 500) / 2 + 'px',
